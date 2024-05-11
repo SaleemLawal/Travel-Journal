5f01c5972085de0850869ab81c4a56d1ebf1cf71
Saleem LawaL <slawal1@umbc.edu>
2024-05-11 09:16:39 -0400

    Create data.jsx
    Main data file

---
+++ src/data.jsx
@@ -0,0 +1,72 @@
+export default [
+  {
+    id: 1,
+    country: "Australia",
+    mapsLink: "",
+    place: "Sydney Opera House",
+    date: "27 May, 2024 - 8 Jun, 2024",
+    description:
+      "The Sydney Opera House is a multi-venue performing arts centre in Sydney. Located on the banks of the Sydney Harbour, it is often regarded as one of the 20th century's most famous and distinctive buildings",
+    img: "https://source.unsplash.com/JmuyB_LibRo",
+  },
+  {
+    id: 2,
+    country: "Japan",
+    mapsLink: "",
+    place: "Mount Fuji",
+    date: "12 Jan, 2025 - 24 Jan, 2025",
+    description:
+      "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
+    img: "https://source.unsplash.com/WLxQvbMyfas",
+  },
+  {
+    id: 3,
+    country: "Norway",
+    mapsLink: "",
+    place: "Geirangerfjord",
+    date: "01 Oct, 2024 - 18 Nov, 2024",
+    description:
+      "The Geiranger Fjord is a fjord in the Sunnmøre region of Møre og Romsdal county, Norway. It is located entirely in the Stranda Municipality.",
+    img: "https://source.unsplash.com/3PeSjpLVtLg",
+  },
+  {
+    id: 4,
+    country: "France",
+    mapsLink: "",
+    place: "Eiffel Tower",
+    date: "15 Mar, 2025 - 29 Mar, 2025",
+    description:
+      "The Eiffel Tower is a wrought-iron lattice tower on the Champ de Mars in Paris, France. It is named after the engineer Gustave Eiffel, whose company designed and built the tower.",
+    img: "https://source.unsplash.com/ApDx4XJzFzY",
+  },
+  {
+    id: 5,
+    country: "USA",
+    mapsLink: "",
+    place: "Grand Canyon",
+    date: "20 Aug, 2024 - 10 Sep, 2024",
+    description:
+      "The Grand Canyon is a steep-sided canyon carved by the Colorado River in Arizona, USA. It is 277 miles long, up to 18 miles wide and attains a depth of over a mile.",
+    img: "https://source.unsplash.com/3RnkZpDqsEI",
+  },
+  {
+    id: 6,
+    country: "Italy",
+    mapsLink: "",
+    place: "Colosseum",
+    date: "05 May, 2025 - 20 May, 2025",
+    description:
+      "The Colosseum, also known as the Flavian Amphitheatre, is an oval amphitheatre in the centre of the city of Rome, Italy. Built of travertine limestone, tuff, and brick-faced concrete, it was the largest amphitheatre ever built at the time and held 50,000 spectators.",
+    img: "https://source.unsplash.com/Oaqk7qqNh_c",
+  },
+  {
+    id: 7,
+    country: "India",
+    mapsLink: "",
+    place: "Taj Mahal",
+    date: "11 Nov, 2024 - 25 Nov, 2024",
+    description:
+      "The Taj Mahal, located in Agra, India, is an ivory-white marble mausoleum on the south bank of the Yamuna river. It was commissioned in 1631 by the Mughal emperor, Shah Jahan, to house the tomb of his favourite wife, Mumtaz Mahal.",
+    img: "https://source.unsplash.com/M6eWvLb2EYY",
+  }
+];
